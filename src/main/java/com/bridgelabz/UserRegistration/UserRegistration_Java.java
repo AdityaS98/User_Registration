e5c0a005c8214bcb82eda571141407928d7391f6
AdityaS98 <adityasharmalko1998@gmail.com>
2023-10-11 11:20:43 +0530

    Added: Program for validating an Email

--- src/main/java/com/bridgelabz/UserRegistration/UserRegistration_Java.java
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistration_Java.java
@@ -7,31 +7,46 @@ import java.util.regex.Pattern;
 public class UserRegistration_Java {
     Scanner sc = new Scanner(System.in);
 
-    public void firstNameValidation() {
+
+        public void firstNameValidation() {
 
 
-        String firstName = sc.next();
+            String firstName = sc.next();
 
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+            Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
 
-        Matcher matcher = pattern.matcher(firstName);
+            Matcher matcher = pattern.matcher(firstName);
 
-        if (matcher.matches()) {
-            System.out.println("Valid Name");
-        } else {
-            System.out.println("Not a valid name");
+            if (matcher.matches()) {
+                System.out.println("Valid Name");
+            } else {
+                System.out.println("Not a valid name");
+            }
         }
-    }
 
-    public void lastNameValidation() {
-        String lastName = sc.next();
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
-        Matcher matcher = pattern.matcher(lastName);
-        if (matcher.matches()) {
-            System.out.println("Valid last name");
-        } else {
-            System.out.println("Invalid last name");
+        public void lastNameValidation() {
+            String lastName = sc.next();
+            Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+            Matcher matcher = pattern.matcher(lastName);
+            if (matcher.matches()) {
+                System.out.println("Valid last name");
+            } else {
+                System.out.println("Invalid last name");
+            }
         }
+
+
+
+    public void emailValidation() {
+        String email = sc.next();
+        Pattern pattern = Pattern.compile("[a][b][c][.][a-z]{1,10}[@][b][l][.][c][o][.][a-z]{1,10}");
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            System.out.println("Valid Email address ");
+        } else {
+            System.out.println("Invalid Email address");
+        }
+
     }
 
 
@@ -40,6 +55,7 @@ public class UserRegistration_Java {
         UserRegistration_Java obj = new UserRegistration_Java();
         obj.firstNameValidation();
         obj.lastNameValidation();
+        obj.emailValidation();
 
     }
 }
