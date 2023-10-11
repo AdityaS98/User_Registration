16bca2e4986727784476ca9579eb3d5d593e0418
AdityaS98 <adityasharmalko1998@gmail.com>
2023-10-11 11:07:36 +0530

    Added: Program for validating first name

--- src/main/java/com/bridgelabz/UserRegistration/UserRegistration_Java.java
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistration_Java.java
@@ -1,7 +1,32 @@
 package com.bridgelabz.UserRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration_Java {
+    Scanner sc = new Scanner(System.in);
+
+    public void firstNameValidation() {
+
+
+        String firstName = sc.next();
+
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.matches()) {
+            System.out.println("Valid Name");
+        } else {
+            System.out.println("Not a valid name");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration Program ");
+
+        UserRegistration_Java obj = new UserRegistration_Java();
+        obj.firstNameValidation();
+        
     }
 }
