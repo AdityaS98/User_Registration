8921dc28308c41a9fa13304172475d4d0c3595ab
AdityaS98 <adityasharmalko1998@gmail.com>
2023-10-11 11:02:35 +0530

    Added: Program for displaying welcome message

---
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistration_Java.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.UserRegistration;
+
+public class UserRegistration_Java {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Program ");
+    }
+}
