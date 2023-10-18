8f7086876bab871029603fb8dda4169b7580b130
AdityaS98 <adityasharmalko1998@gmail.com>
2023-10-18 10:41:09 +0530

    Added: A Program for verifying First Name

--- src/main/java/com/bridgelabz/UserRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistration.java
@@ -5,8 +5,12 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the USer Registration Program");
-        
+    public boolean firstNameValidation(String firstName) throws UserRegistrationException {
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("You have entered Wrong First Name");
+        }
     }
+
 }
